e16a1fd58fb9653036a06ba3f140fc6b94aee81d
Wihito <wihito@gmail.com>
2017-09-14 08:27:22 +0200

    Fleshed out pseudo code TODOs for all functions

--- src/ptp/kademlia.go
+++ src/ptp/kademlia.go
@@ -23,16 +23,39 @@ func (kademlia *Kademlia) LookupContact(target *Contact) {
 			case reply := <- kademlia.replies: //Idle wait for replies to requests
 				// TODO If reply has closer contacts than any in the contact list
 					// TODO Push the new closer contact, pop the furthest
-					// TODO Create and send request to new contact (in a routine)
+					// TODO If it was last reply
+						// TODO Close the loop
+					// TODO Goroutine to create and send request to new contact
 			// TODO Timeout case for when requestees don't reply fast enough (might be disconnected/dead/slow)
+				// TODO Close the loop
+		}
+	}
+	// TODO Return list of closest contacts
+}
+
+func (kademlia *Kademlia) LookupData(hash string) {
+	contacts := kademlia.routingTable.FindClosestContacts(target.ID,3) //Retrieve nodes own closest contacts
+	for _, contact := range contacts { //Send request to nodes own closests contacts for their closests contacts
+		go func() { //Send requests concurrently
+			// TODO Create and send request to contact
+		}()
+	}
+	for {
+		select {
+			case reply := <- kademlia.replies: //Idle wait for replies to requests
+				// TODO If reply has address of data
+					// TODO Download data
+					// TODO Create and send store request to next closest node
+					// TODO Close the loop
+				// TODO If reply has closer contacts than any in the contact list
+					// TODO Push the new closer contact, pop the furthest
+					// TODO Goroutine to create and send request to new contact
+			// TODO Timeout case for when requestees don't reply fast enough (might be disconnected/dead/slow)
+				// TODO Close the loop
 		}
 	}
 }
 
-func (kademlia *Kademlia) LookupData(hash string) {
-	// TODO
-}
-
 func (kademlia *Kademlia) Store(data []byte) {
-	// TODO
+	// TODO Goroutine to create and send store request for data
 }
