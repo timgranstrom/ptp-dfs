91896c3e77a150145489b849ebdaf1b265267c25
niklasfuks <nikfuk-2@student.ltu.se>
2017-10-03 14:22:40 +0200

    Started on CLI
    Started on CLI

--- main/ptpdfs.go
+++ main/ptpdfs.go
@@ -1,42 +1,96 @@
 package main
 
 import (
+
+	"flag"
+	"os"
+	"log"
+	"fmt"
+	"io/ioutil"
 	"github.com/timgranstrom/ptp-dfs"
 	"time"
 )
 
 func main() {
-	/*kademliaInstance1 := ptp.NewKademlia(":8001")
-	kademliaInstance2 := ptp.NewKademlia(":8002")
-	kademliaInstance3 := ptp.NewKademlia(":8003")
-	kademliaInstance3.TestAddContact(*kademliaInstance2.TestGetMeContact())
-	kademliaInstance2.TestAddContact(*kademliaInstance1.TestGetMeContact())
+	storeCommand := flag.NewFlagSet("store", flag.ExitOnError)
+	catCommand := flag.NewFlagSet("cat", flag.ExitOnError)
+	pinCommand := flag.NewFlagSet("pin", flag.ExitOnError)
+	unpinCommand := flag.NewFlagSet("unpin", flag.ExitOnError)
 
-	kademliaInstance1.Run()
-	kademliaInstance2.Run()
-	kademliaInstance3.Run()
-	time.Sleep(time.Second)
-	go kademliaInstance3.TestSendMsg(kademliaInstance1.TestGetMeContact())
+	storeName := storeCommand.String("name", "", "name of the file")
+	catName := catCommand.String("hash", "", "the hash of the file")
+	pinName := pinCommand.String("hash", "", "the file-hash that should be pinned")
+	unpinName := unpinCommand.String("hash", "", "the file-hash that should be pinned")
 
-	for{
+	switch os.Args[1] {
+	case "store":
+		storeCommand.Parse(os.Args[2:])
+
+	case "cat":
+		catCommand.Parse(os.Args[2:])
+
+		log.Println(*catName)
+
+	case "pin":
+		pinCommand.Parse(os.Args[2:])
+
+	case "unpin":
+		unpinCommand.Parse(os.Args[2:])
+
+	default:
+		log.Println("no commands")
+	}
+
+	if unpinCommand.Parsed() {
+		fmt.Println("file that should be unpinned = " + *unpinName)
+		//Call unpin function
+	}
+
+	if pinCommand.Parsed() {
+		fmt.Println("file that should be pinned = " + *pinName)
+		//Call pin function
+	}
+
+	if storeCommand.Parsed() {
+		// call the store function and print the hash out
+		count := 0
+		for i := len(*storeName) - 1; i >= 0; i-- { // iteration through filePath to find only the filename
+			if (string(*storeName)[i]) != 92 { // loop untill \ found
+				count ++
+			} else {
+				break
+			}
+		}
+		startIndex := len(*storeName) - count                 // startindexin orginal string
+		fileName := string(*storeName)[startIndex:]          //slice out the FileName
+		fmt.Println("final sliced string =     " + fileName) // print the final sliced string
+		b, err := ioutil.ReadFile(*storeName)                 // Take out the content of the file in byte
+		if err != nil {
+			fmt.Print(err)
+		}
+		//fmt.Println("File stored = " + *storeName + "  " )
+		fmt.Println(b)
+
+		nodeTest := ptp.NewKademlia(":8001",nil)
+		go nodeTest.Run()
 		time.Sleep(time.Second)
-	}*/
+		nodeTest.Store(fileName,b)
+		time.Sleep(time.Second)
+		// log.Println(kademlia.store(subString,b))
+	}
+
+	if catCommand.Parsed() {
+		// find the path to the data that should be printed out
+		//fileName =: Kademlia.findData(*catName)
+		b, err := ioutil.ReadFile(*catName) // just pass the file name
+		if err != nil {
+			fmt.Print(err)
+		}
+		fmt.Println(b)
+		str := string(b) // convert content to a string
+		fmt.Println(str)
+	}
 }
-
-/*func main() {
-
-	ds := ptp.NewDaemonService()
-	if len(os.Args) > 1 {
-		ds.HandleCommands()
-	}/*else{
-		ds.Start("","") //run on default
-		log.Println("RAN!")
-	}*/
-	/*for{
-		time.Sleep(time.Second)
-		fmt.Printf("RUNNING")
-	}*/
-//}
 
 
 
